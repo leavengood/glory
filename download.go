b3636202250a471b85da85e1fb792510b528305d
Ryan Leavengood <leavengood@gmail.com>
2015-04-22 18:45:00 -0400

    Add a basic download function with tests
    It determines if the file being downloaded is a gzip file and
    automatically unzips it if so.

---
+++ download.go
@@ -0,0 +1,37 @@
+package glory
+
+import (
+	"compress/gzip"
+	"io"
+	"net/http"
+	"strings"
+)
+
+// Download the given URL to the given writer
+func DownloadFile(url string, w io.Writer) error {
+	resp, err := http.Get(url)
+	if err != nil {
+		return err
+	}
+
+	// Unzip while downloading if the file is gzipped
+	var r io.Reader
+	if strings.HasSuffix(url, ".gz") {
+		z, err := gzip.NewReader(resp.Body)
+		if err != nil {
+			return err
+		}
+		defer z.Close()
+		r = z
+	} else {
+		r = resp.Body
+	}
+	defer resp.Body.Close()
+
+	_, err = io.Copy(w, r)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
---
+++ download_test.go
@@ -0,0 +1,46 @@
+package glory
+
+import (
+	"bytes"
+	"compress/gzip"
+	"fmt"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+func downloadFileTestHelper(t *testing.T, urlSuffix, served, expected string) {
+	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, served)
+	}))
+	defer ts.Close()
+
+	b := new(bytes.Buffer)
+	err := DownloadFile(fmt.Sprintf("%s/%s", ts.URL, urlSuffix), b)
+	if err != nil {
+		t.Errorf("Received unexpected error: %v", err)
+	}
+	result := b.String()
+	if result != expected {
+		t.Errorf("Expected: %v, but got: %v", expected, result)
+	}
+}
+
+const FileContents = "An updated file"
+
+func TestDownloadFile_NormalFile(t *testing.T) {
+	downloadFileTestHelper(t, "", FileContents, FileContents)
+}
+
+func TestDownloadFile_GzippedFile(t *testing.T) {
+	expected := FileContents
+
+	// Prepare zipped "file"
+	z := new(bytes.Buffer)
+	gz := gzip.NewWriter(z)
+	gz.Write([]byte(expected))
+	gz.Close()
+	served := string(z.Bytes())
+
+	downloadFileTestHelper(t, "file.gz", served, expected)
+}
